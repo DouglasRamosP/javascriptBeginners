466b7ca6116c4bd876eb791fe090e93822a6d324
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-08 22:44:14 -0300

    function mostrarMenssagem() {   const elemento1 = document.getElementById("menssagem1");   const elemento2 = document.getElementById("menssagem2");   let elemento3 = document.getElementById("menssagem3");   const resultado = document.getElementById("resultado");    elemento1.innerHTML = 3;   elemento2.innerHTML = 2;       const valor1 = parseInt(elemento1.innerHTML);   const valor2 = parseInt(elemento2.innerHTML);    elemento3 = (valor1 + valor2);   resultado.innerHTML = elemento3; }
    Declare três variáveis: a, b e c. Defina valores para a e b. Use operadores para fazer c ser igual a a mais b, depois a menos b, depois a multiplicado por b, e finalmente a dividido por b. Imprima os resultados no console.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 function mostrarMenssagem() {
   const elemento1 = document.getElementById("menssagem1");
   const elemento2 = document.getElementById("menssagem2");
+  let elemento3 = document.getElementById("menssagem3");
   const resultado = document.getElementById("resultado");
 
   elemento1.innerHTML = 3;
-  elemento2.innerHTML = 3;
+  elemento2.innerHTML = 2;
+  
 
   const valor1 = parseInt(elemento1.innerHTML);
   const valor2 = parseInt(elemento2.innerHTML);
 
-  const soma = (valor1 + valor2);
-  resultado.innerHTML = soma;
+  elemento3 = (valor1 + valor2);
+  resultado.innerHTML = elemento3;
 }
