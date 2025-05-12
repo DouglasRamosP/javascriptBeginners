bab073d134b3c1912b427a71a77d0a13cac2705b
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-12 14:12:00 -0300

    Função Básica:
    Escreva uma função chamada 'saudacao' que aceita um argumento chamado 'nome'. A função deve retornar a string "Olá, [nome]!".

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-function mostrarMenssagem() {
-  const elemento1 = document.getElementById("menssagem1");
-  const elemento2 = document.getElementById("menssagem2");
-  const resultado = document.getElementById("resultado");
+function saudacao(nome) {
+  const mostra = document.getElementById("mostra");
 
-  elemento1.innerHTML = 3;
-  elemento2.innerHTML = 3;
-
-  const valor1 = parseInt(elemento1.innerHTML);
-  const valor2 = parseInt(elemento2.innerHTML);
-
-  const soma = (valor1 + valor2);
-  resultado.innerHTML = soma;
+  mostra.innerHTML = `Olá, ${nome}`;
 }
+
+/*
+CASO QUEIRA CHAMAR SEM A NECESSIDADE DE UM BOTÃO USE:
+
+// Chama a função automaticamente ao carregar a página
+window.onload = () => {
+  saudacao("Douglas");
+};
+*/
