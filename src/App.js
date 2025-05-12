34a1af9e36e0c39c91cacffa420744d8584ac330
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-12 18:59:32 -0300

    Manipulação de Objetos
    Crie um objeto chamado estudante com propriedades nome, idade e curso. Em seguida, altere a propriedade idade para um outro valor.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function soma(num1, num2) {
+let estudante = {
+  nome: "Douglas",
+  idade: 27,
+  curso: "Sistemas de Informação",
+};
+
+function altera(estudante) {
   const mostra = document.getElementById("mostra");
 
-  mostra.innerHTML = `A soma dos números é: ${num1 + num2}`;
+  estudante.idade = 28;
+
+  mostra.innerHTML = `
+  <p>Seu objeto foi alterado:</p>
+  <ul>
+    <li>Nome: ${estudante.nome}</li>
+    <li>Idade: ${estudante.idade}</li>
+    <li>Curso: ${estudante.curso}</li>
+  </ul>
+`;
 }
