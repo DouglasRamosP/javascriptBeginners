01cf9f6cafc5dfca339a6c4cc9f0a5983926382a
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-13 15:00:31 -0300

    Manipulando DOM

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function altera() {
+  let titleMuda = document.getElementById('title');
+  let newPara = document.getElementById('mostra')
+
+  titleMuda.textContent = "Novo titulo";
+  newPara.innerHTML = '<p>Este é um novo parágrafo.</p>' ;
+}
