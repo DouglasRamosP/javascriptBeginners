644f2014e6f2feb66c379e0f5905cdd53167137d
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-14 18:50:59 -0300

    Adicionando Eventos a Elementos

--- src/script.js
+++ src/script.js
@@ -1,2 +1,12 @@
-let div = document.getElementById('main');
-div.innerHTML += '<p>Este é um novo parágrafo</p>'
+// seleciona o elemento
+let button = document.getElementById("myButton");
+let paragraph = document.getElementById("myParagraph");
+
+//adicionamos o evento de click ao botão
+button.addEventListener("click", function () {
+  alert("O botão foi clicado!");
+});
+//adicionamos o evento de passae sobre o parágrafo
+paragraph.addEventListener("mouseover", function () {
+  alert("O mouse esta sobre o parágrafo!");
+});
